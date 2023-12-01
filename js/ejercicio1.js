714a656ef7cf0f30b7e014c9b2063f7fac812e22
Sergioig37 <sergioig37@educastur.es>
2023-12-01 19:39:24 +0100

    no estoy seguro de como hacer lo de las flechas :(

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -2,24 +2,96 @@ import Galeria from "./galeria.js";
 window.addEventListener("load", init);
 
 var galeria = new Galeria();
+var contenedorImagen = document.getElementById("imagen");
+var siguiente = document.getElementById("siguiente");
+var anterior = document.getElementById("anterior");
+var primera = document.getElementById("primera");
+var ultima = document.getElementById("ultima");
+
+
 
 function init() {
-    var contenedorImagen = document.getElementById("imagen");
     var imagenAleatoria = galeria.establecerPoscionAleatoria();
-    contenedorImagen.style.backgroundImage = "url" + "(" + "/images/" + imagenAleatoria + ".jpg" + ")";
-    document.getElementById("primera").addEventListener("click", irPrimera);
-    document.getElementById("siguiente").addEventListener("click", irSiguiente);
-    document.getElementById("anterior").addEventListener("click", irAnterior);
-    document.getElementById("ultima").addEventListener("click", irUltima);
+    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenAleatoria + ".jpg)";
+    primera.addEventListener("click", irPrimera);
+    siguiente.addEventListener("click", irSiguiente);
+    anterior.addEventListener("click", irAnterior);
+    ultima.addEventListener("click", irUltima);
+
+  s
+
+
+    ultima.classList.add("habilitado");
+    siguiente.classList.add("habilitado");
+    primera.classList.add("habilitado");
+    anterior.classList.add("habilitado");
+    deshabilitarHabilitar();
 }
 
-function irPrimera(evento) {
+function irPrimera() {
+    let imagenPrimera = galeria.establecerPrimeraPosicion();
+    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenPrimera + ".jpg)";
+    deshabilitarHabilitar();
+}
 
-    if (galeria.Indice === 0) {
-        
+function irSiguiente() {
+    let imagenSiguiente = galeria.posicionSiguiente();
+    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenSiguiente + ".jpg)";
+    deshabilitarHabilitar();
+}
+function irAnterior() {
+    let imagenAnterior = galeria.posicionAnterior();
+    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenAnterior + ".jpg)";
+    deshabilitarHabilitar();
+}
+function irUltima() {
+    let imagenUltima = galeria.establecerUltimaPosicion();
+    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenUltima + ".jpg)";
+    deshabilitarHabilitar();
+}
+
+
+// function irSiguienteKey(evento) {
+   
+//     if (evento.key === "ArrowRight") {
+//         evento.preventDefault();
+//         let imagenSiguiente = galeria.posicionSiguiente();
+//         contenedorImagen.style.backgroundImage = "url" + "(" + "/images/" + imagenSiguiente + ".jpg" + ")";
+//         deshabilitarHabilitar();
+//     }
+
+// }
+// function irAnteriorKey(evento) {
+//     if (evento.key === "ArrowLeft") {
+//         evento.preventDefault();
+//         let imagenAnterior = galeria.posicionAnterior();
+//         contenedorImagen.style.backgroundImage = "url" + "(" + "/images/" + imagenAnterior + ".jpg" + ")";
+//         deshabilitarHabilitar();
+//     }
+
+// }
+
+
+
+function deshabilitarHabilitar() {
+    switch (galeria.Indice) {
+        case (0):
+            primera.className = "deshabilitado";
+            anterior.className = "deshabilitado";
+            ultima.className = "habilitado";
+            siguiente.className = "habilitado";
+            break;
+        case (galeria.Imagenes.length - 1):
+            ultima.className = "deshabilitado";
+            siguiente.className = "deshabilitado";
+            primera.className = "habilitado";
+            anterior.className = "habilitado";
+            break;
+        default:
+            ultima.className = "habilitado";
+            siguiente.className = "habilitado";
+            primera.className = "habilitado";
+            anterior.className = "habilitado";
+
     }
-
-
-
-
 }
--- js/galeria.js
+++ js/galeria.js
@@ -1,45 +1,49 @@
-export default class Galeria{
+export default class Galeria {
 
-    constructor(){
-        this._imagenes = ["foto1","foto2","foto3","foto4","foto5","foto6","foto7"];
+    constructor() {
+        this._imagenes = ["foto1", "foto2", "foto3", "foto4", "foto5", "foto6", "foto7"];
         this._indice = parseInt(0);
     }
 
-    get Imagenes(){
-        return this_imagenes;
+    get Imagenes() {
+        return this._imagenes;
     }
-    get Indice(){
+    get Indice() {
         return this._indice;
     }
-   
-    set Indice(indice){
+
+    set Indice(indice) {
         this._indice = indice;
     }
 
-    establecerPoscionAleatoria(){
-        this._indice = Math.floor(Math.random()*this._imagenes.length);
+    establecerPoscionAleatoria() {
+        this._indice = Math.floor(Math.random() * this._imagenes.length);
         return this._imagenes[this._indice];
     }
 
-    establecerUltimaPosicion(){
-        this._indice = this._imagenes -1;
+    establecerUltimaPosicion() {
+        this._indice = this._imagenes.length - 1;
         return this._imagenes[this._indice];
     }
 
-    establecerPrimeraPosicion(){
+    establecerPrimeraPosicion() {
         this._indice = 0;
         return this._imagenes[0];
     }
-    posicionAnterior(){
-        let posicionActual = this._indice;
-        let posicionAnterior = posicionActual - 1;
-        this._indice = posicionAnterior;
+    posicionAnterior() {
+        if (this._indice !== 0) {
+             let posicionActual = this._indice;
+            let posicionAnterior = posicionActual - 1;
+            this._indice = posicionAnterior;
+        }
         return this._imagenes[this._indice];
     }
-    posicionSiguiente(){
-        let posicionActual = this._indice;
-        let posicionSiguiente = posicionActual + 1;
-        this._indice = parseInt(posicionAnterior);
+    posicionSiguiente() {
+        if(this._indice!==this._imagenes.length-1){
+            let posicionActual = this._indice;
+            let posicionSiguiente = posicionActual + 1;
+            this._indice = parseInt(posicionSiguiente);
+        }
         return this._imagenes[this._indice];
     }
 
