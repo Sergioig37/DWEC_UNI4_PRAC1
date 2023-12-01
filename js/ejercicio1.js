698e0efebbfd73e841da65119295a6ebc8026353
Sergioig37 <sergioig37@educastur.es>
2023-12-01 20:48:49 +0100

    comentarios y mejora importante

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,98 +1,147 @@
+//importamos la clase galeria
 import Galeria from "./galeria.js";
+//y cuando se cargue la página se añade lo necesario
 window.addEventListener("load", init);
 
-var galeria = new Galeria();
+//creamos una galeria con siete imágenes
+//que son todas de las que disponemos
+var galeria = new Galeria(7);
+//pillamos el sitio donde van a ir las imágenes y lo metemos en una variable
 var contenedorImagen = document.getElementById("imagen");
+//y los botones por motivos de comodidad
 var siguiente = document.getElementById("siguiente");
 var anterior = document.getElementById("anterior");
 var primera = document.getElementById("primera");
 var ultima = document.getElementById("ultima");
 
 
-
+//esta función hace todo lo que necesitemos al cargar la página
 function init() {
-    var imagenAleatoria = galeria.establecerPoscionAleatoria();
+    //pillamos una imagen aleatoria
+    var imagenAleatoria = galeria.establecerImagenAleatoria();
+    //y la ponemos en el fondo 
+    //de esta manera
     contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenAleatoria + ".jpg)";
-    primera.addEventListener("click", irPrimera);
-    siguiente.addEventListener("click", irSiguiente);
-    anterior.addEventListener("click", irAnterior);
-    ultima.addEventListener("click", irUltima);
+   
+    //añadimos los eventos a los botones
+    primera.addEventListener("click", irExtremos);
+    siguiente.addEventListener("click", moverse);
+    anterior.addEventListener("click", moverse);
+    ultima.addEventListener("click", irExtremos);
 
-    // siguiente.addEventListener("keydown", irSiguienteKey);
-    // anterior.addEventListener("keydown", irAnteriorKey);
+    //y a la ventana añadimos el evento keydown
 
+    //esto es para que al darle a la flecha izquierda y a la flecha derecha 
+    //podamos movernos de imagen si quisieramos (forma parte del reto extra)
 
+    //tardé un rato en darme cuenta de que era la ventanala que tenía que tener el evento y no los botones
+    window.addEventListener("keydown", moverseKeys);
+    window.addEventListener("keydown", moverseKeys);
+
+    //a todos los botones les damos las clases habilitado por defecto
     ultima.classList.add("habilitado");
     siguiente.classList.add("habilitado");
     primera.classList.add("habilitado");
-    anterior.classList.add("habilitado");
+    anterior.classList.add("habilitado"); 
+     //según en que imagen estemos habilitamos o deshabilitamos los botones necesarios
+     //esto es por si acaso la imagen aleatoria es la primera o la última
+     //así se deshabilitan los botones adecuados
     deshabilitarHabilitar();
 }
 
-function irPrimera() {
-    let imagenPrimera = galeria.establecerPrimeraPosicion();
-    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenPrimera + ".jpg)";
-    deshabilitarHabilitar();
+//esta función sirve para moverse a la imagen siguiente o a la anterior
+//dependiendo del id del evento que reciba el click
+function moverse(evento) {
+    //si es el botón de siguiente
+    if (evento.target.id === siguiente.id) {
+        //vamos a la siguiente imagen y la ponemos
+        let imagenSiguiente = galeria.posicionSiguiente();
+        contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenSiguiente + ".jpg)";
+        //según en que imagen estemos habilitamos o deshabilitamos los botones necesarios
+        deshabilitarHabilitar();
+    }
+    //si es el botón de anterior pues vamos a la imagen anterior
+    else if (evento.target.id === anterior.id) {
+        let imagenAnterior = galeria.posicionAnterior();
+        contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenAnterior + ".jpg)";
+        //según en que imagen estemos habilitamos o deshabilitamos los botones necesarios
+        deshabilitarHabilitar();
+    }
 }
 
-function irSiguiente() {
-    let imagenSiguiente = galeria.posicionSiguiente();
-    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenSiguiente + ".jpg)";
-    deshabilitarHabilitar();
+//esta función es parecida a la anterior
+//pero con última y la primera imagen
+function irExtremos(evento) {
+    //si calcamos al botón de la última imagen la ponemos
+    if (evento.target.id === ultima.id) {
+        let imagenUltima = galeria.establecerUltimaPosicion();
+        contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenUltima + ".jpg)";
+        
+        deshabilitarHabilitar();
+    }
+    //si es el botón de la primera pues ponemos la primera
+    else if (evento.target.id === primera.id) {
+        let imagenPrimera = galeria.establecerPrimeraPosicion();
+        contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenPrimera + ".jpg)";
+        //y vuelta a deshabilutar botones
+        deshabilitarHabilitar();
+    }
 }
-function irAnterior() {
-    let imagenAnterior = galeria.posicionAnterior();
-    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenAnterior + ".jpg)";
-    deshabilitarHabilitar();
+
+//esta función funciona como las anteriores pero con las flechas del teclado
+function moverseKeys(evento) {
+
+    //si es la flecha de la derecha hacemos lo mismo que si le dieramos al botón de siguiente
+    if (evento.key === "ArrowRight") {
+        evento.preventDefault();
+        let imagenSiguiente = galeria.posicionSiguiente();
+        contenedorImagen.style.backgroundImage = "url" + "(" + "/images/" + imagenSiguiente + ".jpg" + ")";
+        deshabilitarHabilitar();
+    }
+    //si es la de la izquierda hacemos lo mismo que si le dieramos al botón de anterior
+    else if (evento.key === "ArrowLeft") {
+        evento.preventDefault();
+        let imagenAnterior = galeria.posicionAnterior();
+        contenedorImagen.style.backgroundImage = "url" + "(" + "/images/" + imagenAnterior + ".jpg" + ")";
+        deshabilitarHabilitar();
+    }
 }
-function irUltima() {
-    let imagenUltima = galeria.establecerUltimaPosicion();
-    contenedorImagen.style.backgroundImage = "url(" + "/images/" + imagenUltima + ".jpg)";
-    deshabilitarHabilitar();
-}
 
 
-// function irSiguienteKey(evento) {
-   
-//     if (evento.key === "ArrowRight") {
-//         evento.preventDefault();
-//         let imagenSiguiente = galeria.posicionSiguiente();
-//         contenedorImagen.style.backgroundImage = "url" + "(" + "/images/" + imagenSiguiente + ".jpg" + ")";
-//         deshabilitarHabilitar();
-//     }
-
-// }
-// function irAnteriorKey(evento) {
-//     if (evento.key === "ArrowLeft") {
-//         evento.preventDefault();
-//         let imagenAnterior = galeria.posicionAnterior();
-//         contenedorImagen.style.backgroundImage = "url" + "(" + "/images/" + imagenAnterior + ".jpg" + ")";
-//         deshabilitarHabilitar();
-//     }
-
-// }
-
-
-
+// y esta última función es para darle los estilos de deshabilitado y habilitado a los botones
 function deshabilitarHabilitar() {
+    //nos basamos según la imagen en la que estemos
+    //y como ya teníamos la clase solo hace falta cambiarle el nombre
     switch (galeria.Indice) {
+        //si estamos en la primera
         case (0):
+            //botones de anterior y primera deshabilitados porque ya estamos en la primera
+            //y porque no podemos ir más hacia atrás
             primera.className = "deshabilitado";
             anterior.className = "deshabilitado";
+            //botones de última y siguiente habilitados porque nos podemos mover hacia adelante
             ultima.className = "habilitado";
             siguiente.className = "habilitado";
+            //y el break para que pare
             break;
+        //si estamos en la última imagen
         case (galeria.Imagenes.length - 1):
+            //no podemos volver a la última ( ya estamos en ella) y no podemos seguir adelante
             ultima.className = "deshabilitado";
             siguiente.className = "deshabilitado";
+            //pero si que podemos ir a la primera e ir hacia atrás
             primera.className = "habilitado";
             anterior.className = "habilitado";
+            //break para no seguir
             break;
+        //y si no es ninguno de los otros dos casos 
         default:
+            //por defecto todo está habilitado
             ultima.className = "habilitado";
             siguiente.className = "habilitado";
             primera.className = "habilitado";
             anterior.className = "habilitado";
+            break;
 
     }
 }
--- js/galeria.js
+++ js/galeria.js
@@ -1,10 +1,30 @@
+//tenemos la clase galería que vamos a exportar
+//y es la que contendrá todas las imágenes
 export default class Galeria {
+    //el constructor recibe el número de imágenes con el que quieres trabajar,
+    //por si quieres meter más imágenes
+    constructor(numeroImagenes) {
+        //creamos el array vacío de imágenes
+        this._imagenes = [];
+        //y según cuantas tengamos hacemos un bucle para meterlas
+        for(let i=1;i<=numeroImagenes;i++){
 
-    constructor() {
-        this._imagenes = ["foto1", "foto2", "foto3", "foto4", "foto5", "foto6", "foto7"];
+            //todas las imágenes tienen el mismo nombre
+            //y solo se diferencian en el número
+
+            var foto = "foto"+i;
+
+            //y se mete dentro del array de imágenes
+            this._imagenes.push(foto);
+        }
+        //este es el índice/puntero que indica la posición en el array
+        //empieza en cero por ningún motivo en especial
+        //y por si acaso lo parseamos a entero para que no haya problemas
         this._indice = parseInt(0);
     }
 
+
+    //getters y setters por si acaso
     get Imagenes() {
         return this._imagenes;
     }
@@ -16,34 +36,47 @@ export default class Galeria {
         this._indice = indice;
     }
 
-    establecerPoscionAleatoria() {
+    //el nombre es bastante descriptivo,
+    //genera una posición aleatoria y devuelve la imagen de esa posición
+    establecerImagenAleatoria() {
         this._indice = Math.floor(Math.random() * this._imagenes.length);
         return this._imagenes[this._indice];
     }
 
+    //este método devuelve la última foto del array
     establecerUltimaPosicion() {
         this._indice = this._imagenes.length - 1;
         return this._imagenes[this._indice];
     }
-
+//este devuelve la primera imagen del array
     establecerPrimeraPosicion() {
         this._indice = 0;
         return this._imagenes[0];
     }
+
+    //método para moverse a la imagen anterior
     posicionAnterior() {
+        //solo está permitido si no estamos en la primera imagen de todas
+        //si no fuera así seguramente daría un error por que se sale del array
         if (this._indice !== 0) {
              let posicionActual = this._indice;
+             //la posición anterior simplemente se saca de restar la actual menos uno
             let posicionAnterior = posicionActual - 1;
             this._indice = posicionAnterior;
         }
+        //devolvemos la imagen que sale
         return this._imagenes[this._indice];
     }
+
+    //mismo método que el anterior pero para la siguiente imagen
     posicionSiguiente() {
+        //excepto si ya estamos en la última que sino se sale del array
         if(this._indice!==this._imagenes.length-1){
             let posicionActual = this._indice;
             let posicionSiguiente = posicionActual + 1;
             this._indice = parseInt(posicionSiguiente);
         }
+        //y se devuelve la imagen
         return this._imagenes[this._indice];
     }
 
